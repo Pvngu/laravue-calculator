a435bac62114dcc650d0fe112b11465660382cad
Pvngu <omargt151102@gmail.com>
2026-04-14 01:56:38 -0700

    chore: update package version to 0.1.0 and add vite-plugin-css-injected-by-js

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,14 @@ import vue from '@vitejs/plugin-vue'
 import { resolve, dirname } from 'path'
 import { fileURLToPath } from 'url'
 import tailwindcss from '@tailwindcss/vite'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 // Safely recreate __dirname for ES Modules
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue(), tailwindcss(), cssInjectedByJsPlugin()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.js'),
